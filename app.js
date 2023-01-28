1759e0f4ca3aa08acb243c130e582937dc289f4f
jesmin-juthi <thesathil67@gmail.com>
2023-01-28 18:12:33 +0600

    JS Practice

---
+++ app.js
@@ -0,0 +1,47 @@
+//* Show output in console 
+
+// console.log('Jesmin Juthi')  
+// console.log("Jesmin Juthi")
+
+//* Variables (Var, Let, Const)
+//! NB: Var is outdated. So try to avoid it 🙂
+
+// let fullName = 'Jesmin Juthi'
+// console.log(fullName);
+
+// let sentence = "Let's learn JS! Let's goooo!";
+// console.log(sentence);
+
+//* Operators -> + - * / % ** 
+//*** 🔥 Tips: How to take input from user? -> 🤔-> using "Prompt() function" 😃 
+//* NB: Prompt returns sting as value  
+
+// let favFruit = prompt("what is your favorite fruit?")
+// console.log(favFruit) 
+
+//* Simple tip calculator 🔥🔥 
+
+// let food = Number( prompt( 'Food Cost? ' ) )
+// let tip = Number( prompt( 'tip% ?' ) / 100 )
+// let tipAmount = food * tip
+// let totalAmount = food + tipAmount
+// console.log('Total Bill: ',totalAmount, 'BDT')
+
+/* * Data Types
+    Numbers 👉 1, 2, 100, 50.5
+    Stings 👉 "Anything in single or double quotes"
+    Boolean 👉 true / false 
+    Array 👉 []
+    Objects 👉 {}
+*/
+
+/* * Math Methods 
+    Floor 👉 Rounds Down -> 5.6 (5)
+    Ceil 👉 Rounds Up -> 5.2 (6)
+    Round 👉 Rounds Nearest Integer -> 5.2 (5) / 5.5 (6)
+    Random 👉 Gives Number Between 0 and 1
+*/
+
+
+
+
